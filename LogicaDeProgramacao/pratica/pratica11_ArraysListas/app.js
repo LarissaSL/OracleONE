1158c0e521d517be0269ed9ded54df789c83041a
LarissaSL <larissa.silvaedge@gmail.com>
2024-01-17 18:28:50 -0300

    EX11 - Listas e funcoes

---
+++ LogicaDeProgramacao/pratica/pratica11_ArraysListas/app.js
@@ -0,0 +1,42 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Hora do Desafio';
+
+let linguagensDeProgramacao = [];
+let nomes = ['Koda', 'Sova', 'Merida'];
+
+function criarListaVazia() {
+    let lista = [];
+    console.log(lista);
+}
+
+function chamarListaDeLinguagens() {
+    linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin' , 'Python'];
+    console.log(linguagensDeProgramacao);
+}
+
+function adicionarNaListaLinguagens() {
+    linguagensDeProgramacao.push('Java');
+    linguagensDeProgramacao.push('Ruby');
+    linguagensDeProgramacao.push('GoLang');
+
+    console.log(linguagensDeProgramacao);
+}
+
+function exibirPrimeiroElemento() {
+    console.log(nomes[0]);
+}
+
+function exibirPrimeiroElemento() {
+    console.log(nomes[0]);
+}
+
+function exibirSegundoElemento() {
+    console.log(nomes[1]);
+}
+
+function exibirUltimoElemento() {
+    console.log(nomes[nomes.length - 1]);
+}
+
+
+
