7e1720c3b53212730df68bf65a1c27d6cbbea644
LarissaSL <larissa.silvaedge@gmail.com>
2024-01-17 15:15:04 -0300

    EX07 - Contagem de 1 a 10 e 10 a 1

---
+++ LogicaDeProgramacao/pratica/pratica07/main.js
@@ -0,0 +1,14 @@
+let contador = 1;
+
+console.log("De 1 a 10:");
+while (contador <= 10) {
+    console.log(contador);
+    contador ++;
+}
+
+console.log("De 10 a 1 agora:");
+
+while (contador != 0) {
+    contador--;
+    console.log(contador);
+}
