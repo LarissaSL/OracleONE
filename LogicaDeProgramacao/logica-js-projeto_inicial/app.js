b7120eb1294d09cfb3555496a1a4d03ce832fc53
LarissaSL <larissa.silvaedge@gmail.com>
2024-01-17 14:14:36 -0300

    V2 - Numero Secreto

--- LogicaDeProgramacao/logica-js-projeto_inicial/app.js
+++ LogicaDeProgramacao/logica-js-projeto_inicial/app.js
@@ -1,8 +1,14 @@
 alert('Boas vindas ao jogo do Número Secreto');
 
-let numeroSecreto = 5;
-let numeroDeChute = prompt('Escolha um número entre 1 e 10');
+let numeroSecreto = 9;
+console.log(`Valor do Número Secreto: ${numeroSecreto}`);
 
-if  (numeroSecreto == numeroDeChute) {
-    console.log('Isso ai! Você descobriu o número secreto (5)');
+let numeroDeChute = prompt('Escolha um número entre 1 e 10');
+console.log(`Valor do Chute: ${numeroDeChute}`)
+
+// Se o NumeroDeChute for igual ao numeroSecreto
+if  (numeroDeChute == numeroSecreto) {
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
+} else {
+    alert('Você errou :(');
 }
